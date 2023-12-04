2dfd8fdd2b4ad0b2bab14cc45e0f92092ad983aa
Aditya-Kanojia <aditya05kanojia@gmial.com>
2023-12-05 01:42:16 +0530

    frequency couter

---
+++ colt/frequencyCounter.js
@@ -0,0 +1,14 @@
+
+
+console.log("Freq Counter");
+
+let arr= [1,2,3,2,4,5,6,2,3,1,1];
+
+obj1={};
+obj2={};
+
+function freqCounter(arr){
+    console.log("freq counter function");
+}
+
+freqCounter()
